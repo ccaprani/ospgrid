ced293128385710b27019962c47997f717cc62c3
ccaprani <ccaprani@users.noreply.github.com>
2022-02-22 14:04:56 +0000

    :art: Format Python code with psf/black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,12 +18,12 @@ sys.path.insert(0, os.path.abspath("../../"))
 
 # -- Project information -----------------------------------------------------
 
-project = 'ospgrid'
-copyright = '2022, Colin Caprani'
-author = 'Colin Caprani'
+project = "ospgrid"
+copyright = "2022, Colin Caprani"
+author = "Colin Caprani"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = "0.1"
 
 
 # -- General configuration ---------------------------------------------------
